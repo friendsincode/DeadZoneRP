cd91c7cee09774fd8b2e87ebd13b5c934aae7327
davedumas0 <davedumas0@yahoo.com>
2018-01-28 14:51:58 -0800

    Update __resource.lua

--- resources/bunkers/__resource.lua
+++ resources/bunkers/__resource.lua
@@ -1,7 +1,6 @@
 
 -- Manifest
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-client_script 'bunkers menu.lua'
-client_script 'bunkers gui.lua'
-client_script 'bunkers client.lua'
+
+client_script 'bunkers.lua'
 client_script 'appbunker.lua'
